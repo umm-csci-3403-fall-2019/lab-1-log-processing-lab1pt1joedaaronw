8729054f7e1d36ffb280f4fa99b274b2a9924e0e
deglm006 <deglm006@morris.umn.edu>
2019-09-10 11:26:57 -0500

    Write parts of create_username_dist.sh
    We're trying to concatenate the files, then count the username occurrences with awk.
    We're not sure what's not working with this. We expect some output to the command line when we run the tests.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+rm -rf username_dist.txt
+for file in $1/*/var/log/failed_login_data.txt; do
+	echo file
+	cat file > bestfilenameever.txt
+done
+echo bestfilenameever.txt
+awk '{count[$4]++} END {for (username in count) print "data.addRow([\x27"username"\x27,"count[username]"]);"}' bestfilenameever.txt > username_dist.txt
+echo username_dist.txt
+wrap_contents.sh username_dist.txt "username_dist" username_dist.html
+echo username_dist.html
