d8b0bcf325ed14d2385b95c87ec234cd1c84efd1
deglm006 <deglm006@morris.umn.edu>
2019-09-10 10:20:35 -0500

    Add wrap_contents.sh
    We used what Joe wrote for the pre-lab.

---
+++ bin/wrap_contents.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Input parameters:
+#1 text file to be wraped
+#2 prefix for header and footer files
+#3 output file
+
+head="${2}_header.html"
+foot="${2}_footer.html"
+
+cat $head $1 $foot > $3
