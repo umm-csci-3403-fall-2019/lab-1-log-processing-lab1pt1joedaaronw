897b406d83facebb9aacb1733006975ad6695b40
deglm006 <deglm006@morris.umn.edu>
2019-09-10 11:59:01 -0500

    Remove extra txt files

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+printf $0
+printf $1
 rm -rf username_dist.txt
+ls $1
 for file in $1/*/var/log/failed_login_data.txt; do
 	echo file
 	cat file > bestfilenameever.txt
 done
 echo bestfilenameever.txt
 awk '{count[$4]++} END {for (username in count) print "data.addRow([\x27"username"\x27,"count[username]"]);"}' bestfilenameever.txt > username_dist.txt
+rm bestfilenameever.txt
 echo username_dist.txt
 wrap_contents.sh username_dist.txt "username_dist" username_dist.html
 echo username_dist.html
+rm username_dist.txt
+
