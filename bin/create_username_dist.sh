994b1060a73bdd71e08114304758f797920c03e6
deglm006 <deglm006@morris.umn.edu>
2019-09-12 11:28:38 -0500

    Fix create_username_dist.sh with sort and uniq
    This makes all the bats tests pass.
    Co-authored-by: Aaron Walter <walte774@morris.umn.edu>

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-echo "please help me"
-echo $0
-echo $1
 rm -rf username_dist.txt
-ls $1
+rm -rf bestfilenameever.txt
+rm -rf username_dist.html
 for file  in $1/*/failed_login_data.txt
 do
-	#file = $dir/failed_login_data.txt
-	echo $file
 	cat $file >> bestfilenameever.txt
 done
-echo bestfilenameever.txt
-awk '{printf $4"\n"}' bestfilenameever.txt > username_dist.txt
-sort "username_dist.txt" | uniq -c >> username_dist.txt
-echo username_dist.txt
+awk '{print $4}' bestfilenameever.txt | sort -fs | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > username_dist.txt
 bin/wrap_contents.sh username_dist.txt "username_dist" $1/username_dist.html
-echo username_dist.html
